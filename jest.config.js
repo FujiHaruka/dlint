ab8f01cf98c663ea2d4c62293c5bb1c071274665
FujiHaruka <fizzyjohn1427@gmail.com>
2019-11-19 18:11:24 +0900

    Modify 'testMatch' in jest config

--- jest.config.js
+++ jest.config.js
@@ -141,7 +141,7 @@ module.exports = {
 
   // The glob patterns Jest uses to detect test files
   testMatch: [
-    "**/test/*.test.[jt]s",
+    "**/test/**/*.test.[jt]s",
   ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
