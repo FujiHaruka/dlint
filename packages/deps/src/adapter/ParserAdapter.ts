45153985c855c9ed526c22ce276d889cd515e65b
FujiHaruka <fizzyjohn1427@gmail.com>
2019-11-23 00:07:23 +0900

    @deps acorn

--- packages/deps/src/adapter/ParserAdapter.ts
+++ packages/deps/src/adapter/ParserAdapter.ts
@@ -1,12 +1,32 @@
 import { FileDepParser } from '../core/dep/FieDepAnalyzer'
 
+// For AST spec, see ESTree https://github.com/estree/estree/blob/master/es2015.md#importdeclaration
+
+interface Statement {
+  type: string
+  source: {
+    value: string
+  }
+}
+
+export interface AcceptableAST {
+  body: Statement[]
+}
+
+export interface Parser {
+  parse(code: string): any
+}
+
+const ImportDeclarationType = 'ImportDeclaration'
+
 export const ParserAdapter = {
-  adapt(parse: (code: string) => any): FileDepParser {
+  adapt(parser: Parser): FileDepParser {
     return {
       parse(code: string): string[] {
-        const ast = parse(code)
+        const ast = parser.parse(code) as AcceptableAST
+
         return ast.body
-          .filter(({ type }: { type: string }) => type === 'ImportDeclaration')
+          .filter((statement: any) => statement.type === ImportDeclarationType)
           .map((statement: any): string => statement.source.value)
       },
     }
--- packages/deps/test/lib/integration/CodeAnalysis.test.ts
+++ packages/deps/test/lib/integration/CodeAnalysis.test.ts
@@ -1,4 +1,5 @@
-import { parse } from '@typescript-eslint/typescript-estree'
+import { parse as tsParse } from '@typescript-eslint/typescript-estree'
+import { parse as acornParse } from 'acorn'
 
 import { ParserAdapter } from '../../../src/adapter/ParserAdapter'
 import { FileDepAnalyzer } from '../../../src/core/dep/FieDepAnalyzer'
@@ -17,47 +18,63 @@ import { MockModuleResolver } from '../../tools/MockResolver'
 
 export const FOO = 1
 `
-  const parser = ParserAdapter.adapt(parse)
-
-  const analyzer = new FileDepAnalyzer({
-    parser,
-    classifier: new ModuleClassifier({
-      resolver: new MockModuleResolver('/project'),
+  const parsers = [
+    ParserAdapter.adapt({
+      parse: (code: string) =>
+        acornParse(code, {
+          sourceType: 'module',
+          ranges: false,
+        }),
     }),
-  })
+    ParserAdapter.adapt({
+      parse: (code: string) => tsParse(code),
+    }),
+  ]
 
-  const dep = analyzer.fromSource('/project/file.ts', code)
-  expect(dep).toEqual({
-    file: '/project/file.ts',
-    fanout: {
-      locals: [
-        {
-          type: ModuleTypes.LOCAL,
-          name: '/foo',
-          path: '/foo',
-        },
-        {
-          type: ModuleTypes.LOCAL,
-          name: '/tools/MockResolver',
-          path: '/tools/MockResolver',
-        },
-      ],
-      packages: [
-        {
-          type: ModuleTypes.PACKAGE,
-          name: 'color',
-        },
-        {
-          type: ModuleTypes.PACKAGE,
-          name: '@typescript-eslint/typescript-estree',
-        },
-      ],
-      builtins: [
-        {
-          type: ModuleTypes.BUILTIN,
-          name: 'fs',
-        },
-      ],
-    },
-  })
+  const analyzers = parsers.map(
+    (parser) =>
+      new FileDepAnalyzer({
+        parser,
+        classifier: new ModuleClassifier({
+          resolver: new MockModuleResolver('/project'),
+        }),
+      }),
+  )
+
+  for (const analyzer of analyzers) {
+    const dep = analyzer.fromSource('/project/file.ts', code)
+    expect(dep).toEqual({
+      file: '/project/file.ts',
+      fanout: {
+        locals: [
+          {
+            type: ModuleTypes.LOCAL,
+            name: '/foo',
+            path: '/foo',
+          },
+          {
+            type: ModuleTypes.LOCAL,
+            name: '/tools/MockResolver',
+            path: '/tools/MockResolver',
+          },
+        ],
+        packages: [
+          {
+            type: ModuleTypes.PACKAGE,
+            name: 'color',
+          },
+          {
+            type: ModuleTypes.PACKAGE,
+            name: '@typescript-eslint/typescript-estree',
+          },
+        ],
+        builtins: [
+          {
+            type: ModuleTypes.BUILTIN,
+            name: 'fs',
+          },
+        ],
+      },
+    })
+  }
 })
