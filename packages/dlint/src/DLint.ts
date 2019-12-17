8bacd06c40c00e9a848ee1cb85e4082251c28309
FujiHaruka <fizzyjohn1427@gmail.com>
2019-12-17 11:18:32 +0900

    packages/dlint DLint class

--- packages/dlint/src/DLint.ts
+++ packages/dlint/src/DLint.ts
@@ -1,16 +1,37 @@
 import { DLintConfig } from '@dlint/config'
-import { DLintConfigFields } from '@dlint/config/build/core/DLintConfigSchema'
+import { DLintLayer } from '@dlint/layer'
+import { DLintRule, LayerRuleBinding } from '@dlint/rule'
 
 export class DLint {
-  config: DLintConfigFields
+  layers: DLintLayer[]
+  rule: DLintRule
 
-  private constructor(config: DLintConfigFields) {
-    this.config = config
+  private constructor(layers: DLintLayer[], rule: DLintRule) {
+    this.layers = layers
+    this.rule = rule
   }
 
   static async init(configPath: string) {
     const config = await DLintConfig.load(configPath)
-    const self = new this(config.fields)
-    return self
+    const rules = config.rules()
+    const { expressions, options } = config.layers()
+    const layers = await Promise.all(
+      expressions.map(({ name, patterns }) =>
+        DLintLayer.gatherDeps(name, patterns, options),
+      ),
+    )
+    const bindings: LayerRuleBinding[] = layers.map((layer) => ({
+      layer,
+      expressions: rules[layer.name] || [],
+    }))
+    const rule = new DLintRule(bindings)
+    const lint = new this(layers, rule)
+    return lint
+  }
+
+  run() {
+    const { rule, layers } = this
+    const disallowed = layers.map((layer) => rule.apply(layer))
+    return disallowed
   }
 }
