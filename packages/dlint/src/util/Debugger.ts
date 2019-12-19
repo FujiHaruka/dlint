1f862affefab531100ad93312675455dc1f9f097
FujiHaruka <fizzyjohn1427@gmail.com>
2019-12-20 00:13:24 +0900

    Fix eslint error

--- packages/dlint/src/util/Debugger.ts
+++ packages/dlint/src/util/Debugger.ts
@@ -3,4 +3,7 @@ import chalk from 'chalk'
 const debug = (...msgs: string[]) =>
   console.log(...[chalk.blue('[DEBUG]'), ...msgs])
 
-export const Debugger = (enabled: boolean) => (enabled ? debug : () => {})
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+const noop = () => {}
+
+export const Debugger = (enabled: boolean) => (enabled ? debug : noop)
