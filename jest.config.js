3742d07fe46f374043395d14c65f78f1096364fe
FujiHaruka <fizzyjohn1427@gmail.com>
2019-12-20 00:17:45 +0900

    Add /test/ for coverage path pattern at jest

--- jest.config.js
+++ jest.config.js
@@ -32,7 +32,8 @@ module.exports = {
 
   // An array of regexp pattern strings used to skip coverage collection
   coveragePathIgnorePatterns: [
-    "/node_modules/"
+    "/node_modules/",
+    "/test/"
   ],
 
   // A list of reporter names that Jest uses when writing coverage reports
