1b726c279966df46eefbe84b6382b09c61fed6cd
FujiHaruka <fizzyjohn1427@gmail.com>
2019-12-21 14:18:59 +0900

    packages/config add test cases

--- packages/config/test/lib/io/ConfigFileReader.test.ts
+++ packages/config/test/lib/io/ConfigFileReader.test.ts
@@ -2,12 +2,12 @@ import { join } from 'path'
 
 import { ConfigFileReader } from '../../../src/io/ConfigFileReader'
 
+const CONFIG_DIR = join(__dirname, '../../fixtures/reader')
+
 it('reads reader/default.yaml', async () => {
   const reader = new ConfigFileReader({})
   const config = await reader.fromPath(
-    await reader.resolveConfigPath(
-      join(__dirname, '../../fixtures/reader/default.yaml'),
-    ),
+    await reader.resolveConfigPath(join(CONFIG_DIR, 'default.yaml')),
   )
   expect(config).toEqual({ foo: 1 })
 })
@@ -17,7 +17,7 @@ it('reads reader/default.yaml as default', async () => {
     defaultFileNames: ['default.yaml'],
   })
   const config = await reader.fromPath(
-    await reader.resolveConfigPath(join(__dirname, '../../fixtures/reader')),
+    await reader.resolveConfigPath(CONFIG_DIR),
   )
   expect(config).toEqual({ foo: 1 })
 })
@@ -27,7 +27,30 @@ it('reads reader/default.json as default', async () => {
     defaultFileNames: ['default.json'],
   })
   const config = await reader.fromPath(
-    await reader.resolveConfigPath(join(__dirname, '../../fixtures/reader')),
+    await reader.resolveConfigPath(CONFIG_DIR),
   )
   expect(config).toEqual({ foo: 1 })
+})
+
+it('errors', async () => {
+  await expect(
+    new ConfigFileReader({
+      defaultFileNames: [],
+    }).resolveConfigPath(CONFIG_DIR),
+  ).rejects.toBeInstanceOf(Error)
+  await expect(
+    new ConfigFileReader({
+      defaultFileNames: [],
+    }).resolveConfigPath(join(CONFIG_DIR, 'default.txt')),
+  ).rejects.toBeInstanceOf(Error)
+  await expect(
+    new ConfigFileReader({
+      defaultFileNames: [],
+    }).resolveConfigPath(join(CONFIG_DIR, 'not-found.txt')),
+  ).rejects.toBeTruthy()
+  await expect(
+    new ConfigFileReader({
+      defaultFileNames: [],
+    }).fromPath(join(CONFIG_DIR, 'default.txt')),
+  ).rejects.toBeInstanceOf(Error)
 })
